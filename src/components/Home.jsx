c201f2d9ba49f0ea7d435456e3a3fe76959be3e2
mishraabhi1987 <73992506+mishraabhi1987@users.noreply.github.com>
2021-03-14 22:46:36 +0530

    fix spelling mistake

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <div className="container">
                 <p className="hometext">
                     <h1 style={{color: "#08BECE", paddingBottom: "15px"}}>Welcome to My Website</h1>
-                    <p>This website is created on React. You will feel the immersive exerience of React JS.</p>
+                    <p>This website is created on React. You will feel the immersive experience of React JS.</p>
                     <b style={{marginBottom: "5px", display: "block"}}>Features you can use:</b>
                     <p>You can explore ToDo App, Notes, Search for Images and Movies, Registration and Users...</p>
                     <input type="submit" onClick={() => navg.push("Todolist")} className="btn btn-outline-warning" value="Checkout" />
