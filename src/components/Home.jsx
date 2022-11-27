5e0428333d72ad4bfb87622d523c07b1699a0c58
mishraabhi1987 <73992506+mishraabhi1987@users.noreply.github.com>
2022-11-27 20:34:37 +0530

    New change test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import img from './Pics/it.png';
-import {useHistory} from 'react-router-dom';
+import React from "react";
+import img from "./Pics/it.png";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
-    const navg = useHistory();
-    console.log(navg);
-    return(
-        <div>
-            <div className="container">
-                <p className="hometext">
-                    <h1 style={{color: "#08BECE", paddingBottom: "15px"}}>Welcome to My Website</h1>
-                    <p>This website is created on React. You will feel the immersive experience of React JS.</p>
-                    <b style={{marginBottom: "5px", display: "block"}}>Features you can use:</b>
-                    <p>You can explore ToDo App, Notes, Search for Images and Movies, Registration and Users...</p>
-                    <input type="submit" onClick={() => navg.push("Todolist")} className="btn btn-outline-warning" value="Checkout" />
-                </p>
-            <img src={img} className="animateimg" />
-            </div>
-        </div>
-    )
-
-}
+  const navg = useHistory();
+  console.log(navg);
+  return (
+    <div>
+      <div className="container">
+        <p className="hometext">
+          <h1 style={{ color: "#08BECE", paddingBottom: "15px" }}>
+            Welcome User to My Website
+          </h1>
+          <p>
+            This website is created on React. You will feel the immersive
+            experience of React JS.
+          </p>
+          <b style={{ marginBottom: "5px", display: "block" }}>
+            Features you can use:
+          </b>
+          <p>
+            You can explore ToDo App, Notes, Search for Images and Movies,
+            Registration and Users...
+          </p>
+          <input
+            type="submit"
+            onClick={() => navg.push("Todolist")}
+            className="btn btn-outline-warning"
+            value="Checkout"
+          />
+        </p>
+        <img src={img} className="animateimg" />
+      </div>
+    </div>
+  );
+};
 export default Home;
